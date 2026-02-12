16881fd12f1877c5282349c77aa4b104f1b1156b
Your Name <your@email.com>
2026-02-12 15:42:35 +0300

    Add SPA rewrite for Vercel

---
+++ android/app/src/main/java/com/kbsoftwares/afrilink/MainActivity.java
@@ -0,0 +1,5 @@
+package com.kbsoftwares.afrilink;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kbsoftwares.afrilink',
+  appName: 'Afrilink',
+  webDir: 'dist'
+};
+
+export default config;
